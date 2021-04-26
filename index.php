f16f38de549089c19090d8b408d7be67f64809cc
jayesh-svg <sikchi.jayesh@gmail.com>
2021-04-26 22:10:35 +0530

    changes

--- index.php
+++ index.php
@@ -87,7 +87,7 @@
    
           <div class="col-lg-7" style="float: right; padding-right: 120px;">
                 <form action="" method="post">
-              <input type="email" name="email"><input type="submit" value="Search">
+              <input type="email" name="email"><input type="submit" value="Search" placeholder="Product title, brand etc.">
             </form>
           </div>
       
@@ -441,7 +441,7 @@
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
             <div class="icon-box">
               <div class="icon"><i class="bx bxl-dribbble"></i></div>
-              <h4><a href="B:/Jayesh/SME%20chamber/BizLand/inner-page.html" target="_BLANK">Technology Scouting</a></h4>
+              <h4><a href="https://demosme.herokuapp.com/inner-page.html" target="_BLANK">Technology Scouting</a></h4>
               <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
             </div>
           </div>
@@ -449,7 +449,7 @@
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-file"></i></div>
-              <h4><a href="B:/Jayesh/SME%20chamber/BizLand/inner-page.html" target="_BLANK">Training Programs</a></h4>
+              <h4><a href="https://demosme.herokuapp.com/inner-page.html" target="_BLANK">Training Programs</a></h4>
               <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
             </div>
           </div>
@@ -457,7 +457,7 @@
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0" data-aos="zoom-in" data-aos-delay="300">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-tachometer"></i></div>
-              <h4><a href="B:/Jayesh/SME%20chamber/BizLand/inner-page.html" target="_BLANK">Promotional Services</a></h4>
+              <h4><a href="https://demosme.herokuapp.com/inner-page.html" target="_BLANK">Promotional Services</a></h4>
               <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
             </div>
           </div>
@@ -465,7 +465,7 @@
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="zoom-in" data-aos-delay="100">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-world"></i></div>
-              <h4><a href="B:/Jayesh/SME%20chamber/BizLand/inner-page.html" target="_BLANK">Company Assessment for Readiness for Exports (CARE)</a></h4>
+              <h4><a href="https://demosme.herokuapp.com/inner-page.html" target="_BLANK">Company Assessment for Readiness for Exports (CARE)</a></h4>
               <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
             </div>
           </div>
@@ -473,7 +473,7 @@
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="zoom-in" data-aos-delay="200">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-slideshow"></i></div>
-              <h4><a href="B:/Jayesh/SME%20chamber/BizLand/inner-page.html" target="_BLANK">Market & Export Information Services</a></h4>
+              <h4><a href="https://demosme.herokuapp.com/inner-page.html" target="_BLANK">Market & Export Information Services</a></h4>
               <p>Quis consequatur saepe eligendi voluptatem consequatur dolor consequuntur</p>
             </div>
           </div>
@@ -481,7 +481,7 @@
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="zoom-in" data-aos-delay="300">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-arch"></i></div>
-              <h4><a href="<a href="B:/Jayesh/SME%20chamber/BizLand/inner-page.html" target="_BLANK">Advisory Services including Due Diligence</a></h4>
+              <h4><a href="<a href="https://demosme.herokuapp.com/inner-page.html" target="_BLANK">Advisory Services including Due Diligence</a></h4>
               <p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>
             </div>
           </div>
